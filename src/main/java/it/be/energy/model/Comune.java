5b0742ca71da4876efad29c62dd679290eed61f4
Antonello Pagnozzi <98751964+Amnendar@users.noreply.github.com>
2022-03-11 23:51:57 +0100

    Cambiata chiave primaria su Comune

--- src/main/java/it/be/energy/model/Comune.java
+++ src/main/java/it/be/energy/model/Comune.java
@@ -3,6 +3,8 @@ package it.be.energy.model;
 import java.util.List;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
@@ -18,6 +20,7 @@ import lombok.NoArgsConstructor;
 public class Comune {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private Long codComune;
 	private String nome;
