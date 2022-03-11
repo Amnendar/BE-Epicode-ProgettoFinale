aa07f20356c94474cf4556136929c716af969419
Antonello Pagnozzi <98751964+Amnendar@users.noreply.github.com>
2022-03-11 21:20:58 +0100

    Creazione Progetto

---
+++ src/main/java/it/be/energy/EnergyServicesApplication.java
@@ -0,0 +1,13 @@
+package it.be.energy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnergyServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnergyServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/be/energy/EnergyServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package it.be.energy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnergyServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
