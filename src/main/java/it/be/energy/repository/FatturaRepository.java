08bcfe7c7e7d8708b912e81d9b1ff50a6c8b3fc3
Antonello Pagnozzi <98751964+Amnendar@users.noreply.github.com>
2022-03-14 10:52:06 +0100

    Creata Repository per Fattura

--- src/main/java/it/be/energy/repository/FatturaRepository.java
+++ src/main/java/it/be/energy/repository/FatturaRepository.java
@@ -1,9 +1,29 @@
 package it.be.energy.repository;
 
+import java.math.BigDecimal;
+import java.util.Date;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import it.be.energy.model.Fattura;
+import it.be.energy.model.StatoFattura;
 
 public interface FatturaRepository extends JpaRepository<Fattura, Long> {
 
+	public Page<Fattura> findAll(Pageable pageable);
+	
+	public Page<Fattura> findByClienteRagioneSocialeLike(Pageable pageable, String nome);
+	
+	public Page<Fattura> findByStato(Pageable pageable, StatoFattura stato);
+	
+	public Page<Fattura> findByData(Pageable pageable, Date data);
+	
+	public Page<Fattura> findByAnno(Pageable pageable, Integer anno);
+	
+	public Page<Fattura> findByImportoBetween(Pageable pageable, BigDecimal minimo, BigDecimal massimo);
+	
+	
+	
 }
